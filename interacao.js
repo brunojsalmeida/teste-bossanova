41e248f4063fabfe77665c75b5b0569618761fc3
Bruno Almeida <45923373+brunojsalmeida@users.noreply.github.com>
2022-06-18 18:09:42 -0300

    Add files via upload

---
+++ interacao.js
@@ -0,0 +1,8 @@
+function abrirMenu(){
+    document.getElementsByClassName('menu-lateral')[0].style.width = '350px';
+}
+
+function fecharMenu(){
+    document.getElementsByClassName('menu-lateral')[0].style.width = '72px';
+}
+
